3bef6369d7f0ecdbfca1fc088284162ba437f510
Flaise <x.Flaise@gmail.com>
2018-02-11 22:04:47 -0600

    Click top left corner to toggle mute.

--- src/music.js
+++ src/music.js
@@ -1,33 +1,47 @@
 const {Howl} = require('howler');
 const {addHandler} = require('./event');
+const {overlapsBounds} = require('./bounds');
+
+const muteBounds = {left: 0, top: 0, right: 50, bottom: 50};
 
 addHandler('load', (session) => {
-    session.music = new Howl({
+    const sound = new Howl({
         src: ['./assets/music.ogg', './assets/music.mp3'],
         sprite: {
             first: [0, 4545],
             loop: [4545, 99004, true], // 103549 = entire track
         },
     });
-    session.music.once('load', () => {
-        session.musicState = 'loaded';
+    session.music = {sound, muted: false};
+    sound.once('load', () => {
+        session.music.state = 'loaded';
     });
-    session.music.once('end', () => {
-        session.music.play('loop');
+    sound.once('end', () => {
+        sound.play('loop');
     });
 });
 
 addHandler('pagevisible', (session, visible) => {
+    if (session.music.muted) return;
     if (visible) {
-        session.music.fade(0, 1, 800);
+        session.music.sound.fade(0, 1, 800);
     } else {
-        session.music.fade(1, 0, 500);
+        session.music.sound.fade(1, 0, 500);
     }
 });
 
-addHandler('mousedown', (session) => {
-    if (session.musicState === 'loaded') {
-        session.music.play('first');
-        session.musicState = 'played';
+addHandler('mousedown', (session, {x, y}) => {
+    if (session.music.state === 'loaded') {
+        session.music.sound.play('first');
+        session.music.state = 'played';
+    }
+
+    if (overlapsBounds(x, y, muteBounds)) {
+        session.music.muted = !session.music.muted;
+        if (session.music.muted) {
+            session.music.sound.fade(1, 0, 500);
+        } else {
+            session.music.sound.fade(0, 1, 800);
+        }
     }
 });
