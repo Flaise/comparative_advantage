04f09cb52f5436a72090838fcca29c56dd641eec
Flaise <x.Flaise@gmail.com>
2018-02-21 08:38:26 -0600

    Using Howler.codecs() function to check audio format support.

--- src/audio.js
+++ src/audio.js
@@ -1,14 +1,7 @@
+const {extname} = require('path');
 const {Howl, Howler} = require('howler');
 const {loadData, startLoading, doneLoading, progressLoading} = require('skid/lib/load');
 const {addHandler} = require('skid/lib/event');
-
-function supportsFormat(format) {
-    const a = document.createElement('audio');
-    return !!(a.canPlayType && a.canPlayType(format).replace(/no/, ''));
-}
-
-const vorbisSupported = supportsFormat('audio/ogg; codecs="vorbis"');
-const mp3Supported = supportsFormat('audio/mpeg;');
 
 function load(state, eventCode, howlArgs, id) {
     return new Promise((resolve, reject) => {
@@ -28,11 +21,11 @@ exports.loadAudio = function loadAudio(state, eventCode, howlArgs) {
     let src = howlArgs.src;
     if (!Array.isArray(src)) src = [src];
     for (const path of src) {
-        if ((path.endsWith('.mp3') && mp3Supported)
-        || (path.endsWith('.ogg') && vorbisSupported)) {
+        const extension = extname(path);
+        if (Howler.codecs(extension)) {
             return loadData(state, path).then((source) => {
                 howlArgs.src = [source];
-                howlArgs.format = [path.substr(path.length - 3)];
+                howlArgs.format = [extension];
                 return load(state, eventCode, howlArgs, id);
             });
         }
